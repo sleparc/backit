e2d9f4a988181484a05ec481fbea44be7fc726f5
Simon Le Parc <simon.leparc@outright.com>
2012-01-31 22:42:42 -0800

    setup test

--- lib/backit/setup.rb
+++ lib/backit/setup.rb
@@ -4,6 +4,8 @@ module Backit
       `cd ~/test_dir/`
       puts "Initializing your repository..."
       `git init`
+      `git add .`
+      `git commit -m 'setup test'`
       puts "Setting up the remote connection..."
       `git remote add origin git@github.com:sleparc/test_dir.git`
       puts "Pushing to master branch..."
